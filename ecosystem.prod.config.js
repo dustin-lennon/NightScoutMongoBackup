370cf357653242f67c67fc470ae40ab254859b98
Dustin Lennon <92093+dustin-lennon@users.noreply.github.com>
2025-11-10 02:26:44 -0500

    formatting issue resolution

--- ecosystem.prod.config.js
+++ ecosystem.prod.config.js
@@ -13,7 +13,7 @@ module.exports = {
       interpreter: 'none',
       instances: 1,
       autorestart: true,
-	  watch: false,
+      watch: false,
       max_memory_restart: '500M',
       error_file: './logs/error.log',
       out_file: './logs/output.log',
